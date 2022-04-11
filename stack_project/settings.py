dba1c662e63d1b3981cb426f29738115d745e9b2
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-04-10 21:36:16 -0400

    config adjust

--- stack_project/settings.py
+++ stack_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p=&ia**@qtxwpzhgv1@$*6al^m3m_2m+n&+5lifb82v@b3_-bl'
+SECRET_KEY = 'fd;jiej;iejiji#$%0280308jffwjdlajodfijiefjdelfjao22'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
